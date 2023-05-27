9468cab524de59f39d49ac3b9f0aa980f448cf2a
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-05-27 19:15:20 -0300

    criado a opção de deletar uma foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -23,11 +23,46 @@ const insertPhoto = async(req, res) => {
   // If photo was createed sucessfully, return data
   if(!newPhoto){
     res.status(422).json({errors: ['Houve um problema, por favor tente novamente mais tarde']});
+    return;
   }
 
   res.send(newPhoto);
 };
 
+// Remove a photo from DB
+const deletePhoto = async (req, res) => {
+  const {id} = req.params;
+
+  const reqUser = req.user;
+
+  try{
+
+    const photo = await Photo.findById(id);
+
+    // Check if photo exists
+    if(!photo){
+      res.status(404).json({errors: ['Foto não encontrada 1']});
+      return;
+    }
+
+    // Check is photo belongs to user
+    if(!photo.userId.equals(reqUser._id)){
+      res.status(422).json({errors: ['Occoreu um erro, por favor tente novamente mais tarde!']});
+      return;
+    }
+
+    await Photo.findByIdAndDelete(photo._id);
+
+    res.status(200).json({id: photo._id, message: 'Foto excluída com sucesso!'});
+
+  } catch(error){
+    res.status(404).json({errors: ['Photo não encontrada 2']});
+    return;
+  }
+
+};
+
 module.exports = {
-  insertPhoto
+  insertPhoto,
+  deletePhoto
 }
