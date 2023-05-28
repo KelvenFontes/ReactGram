57d23345cf421549cc494cdf558e1efcf20bd900
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-05-28 01:17:17 -0300

    atualizando o cors no backend

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -6,11 +6,19 @@ import { Link } from 'react-router-dom';
 // Hooks
 import { useState, useEffect } from 'react';
 
+// Redux
+import { register, reset } from '../../slices/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
 const Register = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const { loading, error } = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +32,14 @@ const Register = () => {
 
     console.log(user);
 
+    dispatch(register(user));
+
   };
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return (
     <div id='register'>
